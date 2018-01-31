a7ada9cd3a29aca5f4e20e87a271ccec3df364ec
su-zhihao <Patrick@192.168.0.28>
2018-01-31 03:49:55 -0500

    finish the code for randomizing the mines

--- GridGameGamble/src/application/Minesweeper.java
+++ GridGameGamble/src/application/Minesweeper.java
@@ -18,8 +18,8 @@ public class Minesweeper extends Cell{
 	private final int COLUMNS = 10;
 	// initialize the width of each grid
 	private final int w = 50;
-	// number of mines
-	private final int mines = 10;
+	// just a number code to show that there is a MINE
+	private final int MINE = 10;
 	
 	//Grid Buttons
 	Button[][] Cell = new Button[ROWS][COLUMNS];
@@ -39,6 +39,8 @@ public class Minesweeper extends Cell{
 			}
 		}
 		
+		createMines();
+		
 		// set up the scene and the stage
 		Scene scene1 = new Scene(grid, w * ROWS, w * COLUMNS); //500*500
 		Stage stage = new Stage();
@@ -47,7 +49,64 @@ public class Minesweeper extends Cell{
 	}
 	
 	public void createMines(){
-		
+		// store all the positions in an arraylist
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i < counts.length; i++){
+			for(int j = 0; j < counts.length; j++){
+				list.add(i * 100 + j);
+			}
+		}
+		// randomly pick 10 of them
+		for(int i = 0; i < 10; i++){
+			//randomly select a position
+			int choice = (int)(Math.random() * list.size()); 
+			int r = list.get(choice) / 100; // the row
+			int c = list.get(choice) % 100; // the coloumn 
+			counts[r][c] = MINE; 
+			list.remove(choice); // remove to avoid picking again
+		}
+		// initialize the neighbour count
+		for(int x = 0; x < counts.length; x++){
+			for(int y = 0; y < counts.length; y++){
+				// check if it is a mine
+				if(counts[x][y] != MINE){ 
+					int neighbourcount = 0;
+					//top left
+					if(x > 0 && y > 0 && counts[x-1][y-1] == MINE){ 
+						neighbourcount++;
+					}
+					//top
+					if(y > 0 && counts[x][y-1] == MINE){ 
+						neighbourcount++;
+					}
+					//top right
+					if(x < counts.length - 1 && y > 0 && counts[x+1][y-1] == MINE){ 
+						neighbourcount++;
+					}
+					//left
+					if(x > 0 && counts[x-1][y] == MINE){ 
+						neighbourcount++;
+					}
+					//right
+					if(x < counts.length - 1 && counts[x+1][y] == MINE){ 
+						neighbourcount++;
+					}
+					//bottom left
+					if(x > 0 && y < counts.length - 1 && counts[x-1][y+1] == MINE){ 
+						neighbourcount++;
+					}
+					//bottom
+					if(y < counts.length - 1 && counts[x][y+1] == MINE){ 
+						neighbourcount++;
+					}
+					//bottom right
+					if(x < counts.length - 1 && y < counts.length - 1 && counts[x+1][y+1] == MINE){ 
+						neighbourcount++;
+					}
+					counts[x][y] = neighbourcount;
+				}
+			}
+		}
 	}
 	
 	public static void main(String[] arg){
